e51e0d8a04584d5a2cdef403c344067e40f986d9
sumitpant190 <114337820+sumitpant190@users.noreply.github.com>
2023-06-02 22:42:38 +0545

    first commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_zoom_clone/resources/auth_methods.dart';
+import 'package:flutter_zoom_clone/widgets/custom_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final AuthMethods _authMethods = AuthMethods();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Start or join a meeting',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+              padding: EdgeInsets.symmetric(vertical: 35),
+              child: Image.asset('assets/images/onboarding.jpg')),
+          CustomButton(
+            text: 'Login',
+            onPressed: () async {
+              bool res = await _authMethods.signInWithGoogle(context);
+              if (res) {
+                Navigator.pushNamed(context, '/home');
+              }
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
